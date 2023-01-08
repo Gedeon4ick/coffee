56283798e31579d22152d64b8e5051147b228c84
Gedeon4ick <pavell720@gmaul.com>
2023-01-08 15:42:55 +0600

    filter

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './myInput.scss'
 
-const MyInput = () => {
-    const [filter, setFilter] = React.useState("");
-    console.log(filter);
+const MyInput = ({ onInputChange, filter, currentCountry, country }) => {
+
+    
     return (
         <div className='cont'>
             <div className="input">
                 <label>Lookiing for</label>
                 <input 
                     value={filter}
-                    onChange={({ target: { value } }) => setFilter(value)}
+                    onChange={(e) => onInputChange(e.target.value)}
                     placeholder="start typing here..."
                     type="text"
                 />
@@ -20,9 +20,11 @@ const MyInput = () => {
                     Or filter
                 </div>
                 <div className="filter__btn">
-                    <button>Brazil</button>
-                    <button>Kenya</button>
-                    <button>Columbia</button>
+                    <button value={"Brazil"} className={ country=="Brazil" ? "active" : null } onClick={(e) => {
+                        currentCountry(e.target.value)
+                    }}>Brazil</button>
+                    <button value={"Kenya"} className={ country=="Kenya" ? "active" : null }  onClick={(e) => currentCountry(e.target.value)}>Kenya</button>
+                    <button value={"Columbia"} className={ country=="Columbia" ? "active" : null }  onClick={(e) => currentCountry(e.target.value)}>Columbia</button>
                 </div>
             </div>
         </div>
--- src/components/cardPage/CardList.jsx
+++ src/components/cardPage/CardList.jsx
@@ -4,16 +4,42 @@ import Card from '../../components/card/Card';
 import MyInput from '../UI/input/MyInput';
 
 const CardPage = ({arr, titleLine, isBest}) => {
-    const [state, setState] = useState(arr);
-    console.log(state);
+    
+    const [filter, setFilter] = useState("");
+    const [country, setCountry] = useState("");
+
+    let data = arr;
+
+    const changeCountry = (e) => {
+        if (country === e) {
+            setCountry("")
+        } else {
+            setCountry(e)
+        }
+    }
+
+    if (filter !== '') {
+        data = data.filter(element => element.title.toLowerCase().includes(filter.toLowerCase()))
+    }
+    if (country !== '') {
+        data = data.filter(element => element.country.includes(country))
+    }
 
     return (
+        
         <div className={isBest ? 'cardPage ' + "best" : "cardPage" }>
             { titleLine ? <div className='line'></div> : <div className='title'>Our best</div> }
-            { titleLine ? <MyInput/> : null }
+            { titleLine ? 
+            <MyInput 
+                filter={filter}
+                onInputChange={setFilter}
+                currentCountry={changeCountry}
+                country={country} 
+            /> 
+            : null }
             <div className="container">
                 <div className="wr">
-                    {state.map((element) => {
+                    {data.map((element) => {
                         if (isBest) {
                             if (element.isBest) {
                                 return<Card  element={element} key={element.id}/>
